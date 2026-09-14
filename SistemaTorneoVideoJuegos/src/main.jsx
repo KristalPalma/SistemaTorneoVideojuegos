ad8b240705b8ce188e2885130e3d9c4535ab65a8
IdenyLR <idenylr@gmail.com>
2026-09-13 21:31:28 -0600

    This reverts commit c1c2d87b77c1f0ce2f41d7191f21be1b69775d70, reversing changes made to 150d4ed34bf314520a97426569859056d94d773b.

--- SistemaTorneoVideoJuegos/src/main.jsx
+++ SistemaTorneoVideoJuegos/src/main.jsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-// Aqui pongo App dentro del div root de index.html.
-// StrictMode ayuda a detectar problemas durante el desarrollo.
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
